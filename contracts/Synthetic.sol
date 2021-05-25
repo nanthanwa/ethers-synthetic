79cf76c2a600ce0bd038220115f3a1f9e4355166
Thanwa J <thanwa.jdr@gmail.com>
2021-05-25 19:42:02 +0700

    Finish mint asset

--- contracts/Synthetic.sol
+++ contracts/Synthetic.sol
@@ -24,8 +24,8 @@ contract Synthetic is Ownable {
     mapping(address => string) public addressToPairs;
 
     uint256 public denominator = 1e18; // 1 scaled by 1e18
-    uint256 public liquidationRatio = 1e18 + 75e16; // 1.75 scaled by 1e18
-    uint256 public collateralRatio = 1e18 + 5e17; // 1.5 scaled by 1e18
+    uint256 public collateralRatio = 1e18 + 5e17; // 1.5 scaled by 1e18 (> 1.5 is good)
+    uint256 public liquidationRatio = 1e18 + 25e16; // 1.25 scaled by 1e18
 
     struct MintingNote {
         address minter;
@@ -34,13 +34,32 @@ contract Synthetic is Ownable {
         uint256 assetAmount;
         uint256 assetBackedAmount;
         uint256 currentRatio;
+        uint256 willLiquidateAtPrice;
         uint256 canMintRemainning;
         uint256 canWithdrawRemainning;
-        uint256 startMintingDate;
+        uint256 updatedAt;
+        uint256 updatedBlock;
         uint256 pastDays;
+        uint256 exchangeRateAtMinted;
     }
 
     mapping(address => mapping(address => MintingNote)) public minter; // lender => asset => MintingNote
+
+    event MintAsset(
+        address minter,
+        address indexed syntheticAddress,
+        uint256 amount
+    );
+    event RedeemAsset(address indexed syntheticAddress, uint256 amount);
+    event addCollateral(address indexed user, uint256 amount);
+    event removeCollateral(address indexed user, uint256 amount);
+    event liquidated(
+        address indexed liquidated,
+        address indexed liquidator,
+        address indexed syntheticAddress,
+        uint256 amount,
+        uint256 timestamp
+    );
 
     constructor(IERC20 _dolly, IStdReference _ref) public {
         dolly = _dolly; // use Dolly as collateral
@@ -59,30 +78,80 @@ contract Synthetic is Ownable {
         uint256 _backedAmount // amount of Dolly that you want to collateral
     ) external {
         MintingNote storage mn = minter[_msgSender()][address(_synthetic)];
-        require(mn.minter == address(0));
+        require(
+            mn.minter == address(0),
+            "Synthetic::mintSynthetic: transfer to address(0)"
+        );
 
         uint256 exchangeRate = getRate(addressToPairs[address(_synthetic)]);
         uint256 assetBackedAtRateAmount =
-            (mn.assetAmount * exchangeRate) / denominator;
+            (_amount * exchangeRate) / denominator; // 606872500000000000000
+        uint256 requiredAmount =
+            (assetBackedAtRateAmount * collateralRatio) / denominator;
         require(
-            _backedAmount >=
-                (assetBackedAtRateAmount * collateralRatio) / denominator
+            _backedAmount >= requiredAmount,
+            "Synthetic::mintSynthetic: under collateral"
         );
-        _synthetic.mint(_msgSender(), _backedAmount);
+        uint256 canWithdrawRemainning = _backedAmount - requiredAmount;
+        _synthetic.mint(_msgSender(), _amount);
 
         require(dolly.transferFrom(_msgSender(), address(this), _backedAmount));
         mn.minter = _msgSender();
         mn.asset = _synthetic;
+        mn.assetBacked = dolly;
         mn.assetAmount = _amount;
         mn.assetBackedAmount = _backedAmount;
+        mn.canWithdrawRemainning = canWithdrawRemainning;
+        mn.canMintRemainning =
+            (((canWithdrawRemainning * denominator) / exchangeRate) *
+                denominator) /
+            denominator;
+        mn.exchangeRateAtMinted = exchangeRate;
+        mn.currentRatio =
+            (((_backedAmount * denominator) / exchangeRate) * denominator) /
+            denominator; // must more than 1.5 ratio (15e17)
+        mn.willLiquidateAtPrice =
+            (_backedAmount * liquidationRatio) /
+            denominator; // more assetBacked, more liquidatePrice
+        mn.updatedAt = block.timestamp;
+        mn.updatedBlock = block.number;
+        emit MintAsset(_msgSender(), address(_synthetic), _amount);
     }
 
-    // user need to approve for burn at SyntheticAsset before call this function.
+    // minter needs to approve for burn at SyntheticAsset before call this function.
+    // ne need to redeem entire colateral amount
     function redeemSynthetic(IERC20Burnable _synthetic, uint256 _amount)
         external
     {
-        _synthetic.burnFrom(_msgSender(), _amount);
-        dolly.transfer(_msgSender(), 1 ether);
+        MintingNote storage mn = minter[_msgSender()][address(_synthetic)];
+
+        require(
+            mn.assetAmount >= _amount,
+            "Synthetic::redeemSynthetic: amount exceeds collateral"
+        );
+        uint256 percent = (_amount / mn.assetAmount) * denominator;
+        uint256 assetToBeBurned =
+            (mn.assetAmount * (denominator - percent)) / denominator;
+        uint256 assetBackedToBeRedeemed =
+            (mn.assetBackedAmount * (denominator - percent)) / denominator;
+
+        _synthetic.burnFrom(_msgSender(), assetToBeBurned);
+        dolly.transfer(_msgSender(), assetBackedToBeRedeemed);
+
+        uint256 exchangeRate = getRate(addressToPairs[address(_synthetic)]);
+
+        mn.assetAmount = mn.assetAmount - assetToBeBurned;
+        mn.assetBackedAmount = mn.assetBackedAmount - assetBackedToBeRedeemed;
+        mn.willLiquidateAtPrice =
+            (mn.assetBackedAmount / liquidationRatio) *
+            denominator;
+        mn.currentRatio = (mn.assetBackedAmount / exchangeRate) * denominator; // must more than 1.5 ratio
+        // mn.canWithdrawRemainning = canWithdrawRemainning;
+        // mn.canMintRemainning =
+        //     (canWithdrawRemainning * exchangeRate) /
+        //     denominator;
+        mn.updatedAt = block.timestamp;
+        mn.updatedBlock = block.number;
     }
 
     function liquidate() external {}
--- scripts/deploy-initialized.js
+++ scripts/deploy-initialized.js
@@ -6,7 +6,9 @@ async function main() {
 
   if (network.name === 'kovan') {
     let result;
-    const dolly = await ethers.getContractAt('contracts/Dolly.sol:Dolly', '0x172018C14eeA6312BF4243BFa3d07249feA7E542');
+    const dollyAddress = '0x172018C14eeA6312BF4243BFa3d07249feA7E542';
+    const bandRef = '0x9106f09bf08dfb23fca61a9829543f1c80a81a4b';
+    const dolly = await ethers.getContractAt('contracts/Dolly.sol:Dolly', dollyAddress);
     await dolly.deployed();
     console.log('Dolly deployed to:', dolly.address);
     // console.log('verify Synthetic');
@@ -19,19 +21,19 @@ async function main() {
     //   ],
     // });
 
-    // // const Synthetic = await ethers.getContractFactory('Synthetic');
-    // // const synthetic = await Synthetic.deploy('0x172018C14eeA6312BF4243BFa3d07249feA7E542', '0x9106f09bf08dfb23fca61a9829543f1c80a81a4b'); // dolly, bandRef
-    const synthetic = await ethers.getContractAt('Synthetic', '0xC2620Df1a74175B94Fc54574FF7c287da4A8fF29');
+    const Synthetic = await ethers.getContractFactory('Synthetic');
+    const synthetic = await Synthetic.deploy(dolly.address, bandRef); // dolly, bandRef
+    // const synthetic = await ethers.getContractAt('Synthetic', '0x7176ac825A9BdbfdC203a96a66d48479C2D5af0C');
     await synthetic.deployed();
     console.log('Synthetic deployed to:', synthetic.address);
-    // console.log('verify Synthetic');
-    // await run('verify:verify', {
-    //   address: synthetic.address,
-    //   constructorArguments: [
-    //     '0x172018C14eeA6312BF4243BFa3d07249feA7E542',
-    //     '0x9106f09bf08dfb23fca61a9829543f1c80a81a4b',
-    //   ],
-    // });
+    console.log('verify Synthetic');
+    await run('verify:verify', {
+      address: synthetic.address,
+      constructorArguments: [
+        dolly.address,
+        bandRef,
+      ],
+    });
     // // const DoppleTSLA = await ethers.getContractFactory('DoppleTSLA');
     // // const doppleTSLA = await DoppleTSLA.deploy(synthetic.address); // synthetic address
     const doppleTSLA = await ethers.getContractAt('DoppleTSLA', '0xdC47e2C0b6046cD58d3B21583DA19B45Ebf679ad');
@@ -45,20 +47,23 @@ async function main() {
     //   ],
     // });
 
-    // console.log('setting setPairsToAddress');
-    // result = await synthetic.setPairsToAddress('TSLA/USD', doppleTSLA.address);
-    // console.log('hash', result.hash);
-    // console.log('setting setAddressToPairs');
-    // result = await synthetic.setAddressToPairs(doppleTSLA.address, 'TSLA/USD');
-    // console.log('hash', result.hash);
+    console.log('setting setPairsToAddress');
+    result = await synthetic.setPairsToAddress('TSLA/USD', doppleTSLA.address);
+    console.log('hash', result.hash);
+    console.log('setting setAddressToPairs');
+    result = await synthetic.setAddressToPairs(doppleTSLA.address, 'TSLA/USD');
+    console.log('hash', result.hash);
 
 
-    // console.log('approve dolly');
-    // result = await dolly.approve(synthetic.address, ethers.utils.parseEther('100').toString());
-    // console.log('hash', result.hash);
-    // console.log('approve doppleTSLA');
-    // result = await doppleTSLA.approve(synthetic.address, ethers.utils.parseEther('100').toString());
-    // console.log('hash', result.hash);
+    console.log('approve dolly');
+    result = await dolly.approve(synthetic.address, ethers.utils.parseEther('1000000000000').toString()); // for minting purpose.
+    console.log('hash', result.hash);
+    console.log('approve doppleTSLA');
+    result = await doppleTSLA.approve(synthetic.address, ethers.utils.parseEther('1000000000000').toString()); // for redeem purpose (burned by synthetic contract).
+    console.log('hash', result.hash);
+    console.log('Set owner of systetic asset');
+    result = await doppleTSLA.setSyntheticAddress(synthetic.address); // only Synthetic contract can mint synthetic asset.
+    console.log('hash', result.hash);
 
   }
   else {

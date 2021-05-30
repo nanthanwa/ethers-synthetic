23633722582ad06d15e4986f1d9eb60949ecdefa
Thanwa J <thanwa.jdr@gmail.com>
2021-05-30 23:23:58 +0700

    Fix deploy constructor

--- test/1_deploy_contracts.js
+++ test/1_deploy_contracts.js
@@ -2,7 +2,11 @@ const { assert } = require('chai');
 
 describe('Deploy Contract', () => {
   const bandRef = '0xDA7a001b254CD22e46d3eAB04d937489c93174C3'; // use proxy
-  let dolly, synthetic, doppleSyntheticToken, doppleSyntheticTokenFactory;
+  let deployer, dolly, synthetic, doppleSyntheticToken, doppleSyntheticTokenFactory;
+  before(async () => {
+    const namedAccounts = await getNamedAccounts();
+    deployer = namedAccounts.deployer;
+  });
 
   it('Can deploy Dolly contract', async () => {
     const Dolly = await ethers.getContractFactory("Dolly");
@@ -35,7 +39,7 @@ describe('Deploy Contract', () => {
   });
 
   it('Can deploy DoppleTSLA contract', async () => {
-    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic TSLA Token', 'dTSLA', synthetic.address);
+    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic TSLA Token', 'dTSLA', deployer, synthetic.address);
     const contractAddress = (await tx.wait()).events[2].args[0];
     const assetName = (await tx.wait()).events[2].args[1].substr(1);
     console.log('contractAddress', contractAddress);
@@ -43,7 +47,7 @@ describe('Deploy Contract', () => {
   });
 
   it('Can deploy DoppleCOIN contract', async () => {
-    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic COIN Token', 'dCOIN', synthetic.address);
+    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic COIN Token', 'dCOIN', deployer, synthetic.address);
     const contractAddress = (await tx.wait()).events[2].args[0];
     const assetName = (await tx.wait()).events[2].args[1].substr(1);
     console.log('contractAddress', contractAddress);
@@ -51,7 +55,7 @@ describe('Deploy Contract', () => {
   });
 
   it('Can deploy DoppleAAPL contract', async () => {
-    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic AAPL Token', 'dAAPL', synthetic.address);
+    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic AAPL Token', 'dAAPL', deployer, synthetic.address);
     const contractAddress = (await tx.wait()).events[2].args[0];
     const assetName = (await tx.wait()).events[2].args[1].substr(1);
     console.log('contractAddress', contractAddress);
@@ -59,7 +63,7 @@ describe('Deploy Contract', () => {
   });
 
   it('Can deploy DoppleQQQ contract', async () => {
-    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic QQQ Token', 'dQQQ', synthetic.address);
+    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic QQQ Token', 'dQQQ', deployer, synthetic.address);
     const contractAddress = (await tx.wait()).events[2].args[0];
     const assetName = (await tx.wait()).events[2].args[1].substr(1);
     console.log('contractAddress', contractAddress);
@@ -67,7 +71,7 @@ describe('Deploy Contract', () => {
   });
 
   it('Can deploy DoppleAMZN contract', async () => {
-    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic AMZN Token', 'dAMZN', synthetic.address);
+    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic AMZN Token', 'dAMZN', deployer, synthetic.address);
     const contractAddress = (await tx.wait()).events[2].args[0];
     const assetName = (await tx.wait()).events[2].args[1].substr(1);
     console.log('contractAddress', contractAddress);
@@ -75,7 +79,7 @@ describe('Deploy Contract', () => {
   });
 
   it('Can deploy DoppleXAU contract', async () => {
-    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic XAU Token', 'dXAU', synthetic.address);
+    const tx = await doppleSyntheticTokenFactory.createSyntheticToken('Dopple Synthetic XAU Token', 'dXAU', deployer, synthetic.address);
     const contractAddress = (await tx.wait()).events[2].args[0];
     const assetName = (await tx.wait()).events[2].args[1].substr(1);
     console.log('contractAddress', contractAddress);
